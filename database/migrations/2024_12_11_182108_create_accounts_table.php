7c8ce0dbedd3a6bcc2e91ee6dec1ed4f564bef45
fguilherme25 <fguilherme25@gmail.com>
2024-12-14 14:56:18 -0300

    Inicial

---
+++ database/migrations/2024_12_11_182108_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bank_id')->constrained();
+            $table->integer('agency');
+            $table->integer('number');
+            $table->integer('digit');
+            $table->string('type', length: 2);
+            $table->double('limit');
+            $table->double('balance');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
---
+++ database/migrations/2024_12_11_182203_create_operations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('operations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->datetime('date');
+            $table->string('type', length: 2);
+            $table->string('description', length: 100);
+            $table->double('amount');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('operations');
+    }
+};
