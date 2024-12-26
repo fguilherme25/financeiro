a580ae17765764d6b8cc7e418f18ae94cd7b1db4
fguilherme25 <fguilherme25@gmail.com>
2024-12-26 19:49:46 -0300

    SB Layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,10 +6,83 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Financeiro - @yield('title')</title>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/all.min.css') }}" rel="stylesheet">
 </head>
-<body>
-    @yield('content')
+<body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="#">Financeiro</a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Perfil</a></li>
+                        <li><a class="dropdown-item" href="#!">Avisos</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="#!">Sair</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Sistema</div>
+                            <a class="nav-link" href="{{ route('operation.index') }}">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Movimentações
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Cadastro</div>
+                            <a class="nav-link" href="{{ route('category.index') }}">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-list"></i></i></div>
+                                Categorias
+                            </a>
+                            <a class="nav-link" href="{{ route('expense.index') }}">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-money-check-dollar"></i></i></div>
+                                Despesas
+                            </a>
+                            <a class="nav-link" href="{{ route('bank.index') }}">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-building-columns"></i></div>
+                                Bancos
+                            </a>
+                            <a class="nav-link" href="{{ route('account.index') }}">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-receipt"></i></i></div>
+                                Contas
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Usuário:</div>
+                        Fabio Francisco
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid mt-4 px-4">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                @yield('content')
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        
+        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('js/scripts.js') }}"></script>
+        <script src="{{ asset('js/all.min.js') }}"></script>
+    </body>
 
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-</body>
+
 </html>
